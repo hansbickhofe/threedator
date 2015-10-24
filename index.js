7307214bcbb28af11b047b130a8839cd3cda8014
Hans Bickhofe <hbickhofe@gmail.com>
2015-10-24 22:26:16 +0200

    cleanup

--- index.js
+++ index.js
@@ -6,19 +6,9 @@
 * Author : TeYoU
 * pudding [dot] pearl [dot] tea [at] gmail [dot] com
 **/
-var express = require('express');
-var app = express();
-var http = require('http').createServer(app);
+
 var io = require('socket.io')(http);
 var PORT = process.env.PORT || 3000;
-
-//static
-app.use(express.static(__dirname + '/public'));
-
-app.get('/',function(req,res){
-	//res.send('<h1>Hello World</h1>');
-	res.sendFile(__dirname + '/public/index.html');
-});
 
 io.on('connection', function(socket){
 
@@ -32,7 +22,7 @@ io.on('connection', function(socket){
     }
     io.emit('channelname',msg);
   });
-  
+
 });
 
 
