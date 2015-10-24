c25d4e3b7395d1b08b96593a80a1602ad79d5450
Hans Bickhofe <hbickhofe@gmail.com>
2015-10-24 23:18:39 +0200

    Revert "cleanup"
    This reverts commit 7307214bcbb28af11b047b130a8839cd3cda8014.

--- index.js
+++ index.js
@@ -6,9 +6,19 @@
 * Author : TeYoU
 * pudding [dot] pearl [dot] tea [at] gmail [dot] com
 **/
-
+var express = require('express');
+var app = express();
+var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 var PORT = process.env.PORT || 3000;
+
+//static
+app.use(express.static(__dirname + '/public'));
+
+app.get('/',function(req,res){
+	//res.send('<h1>Hello World</h1>');
+	res.sendFile(__dirname + '/public/index.html');
+});
 
 io.on('connection', function(socket){
 
@@ -22,7 +32,7 @@ io.on('connection', function(socket){
     }
     io.emit('channelname',msg);
   });
-
+  
 });
 
 
