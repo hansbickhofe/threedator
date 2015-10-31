f8d82ba898f0b69845871fe0dcbfbc42afbb44a7
Hans Bickhofe <hbickhofe@gmail.com>
2015-11-01 00:56:07 +0100

    ndb class for players

--- appengine/ndbutil.py
+++ appengine/ndbutil.py
@@ -12,7 +12,7 @@ from google.appengine.api import memcache
 TEMPLATES_DIR = os.path.join(os.path.dirname(__file__), 'jinja2_templates')
 JINJA_ENVIRONMENT = jinja2.Environment(loader = jinja2.FileSystemLoader(TEMPLATES_DIR),autoescape = True)
 
-DEFAULT_SHIP = 'Ship'
+DEFAULT_SHIP = 'Player'
 DEFAULT_OBJECT = 'Object'
 
 class Settings():
@@ -25,125 +25,120 @@ class Settings():
 	timeoutShowShip = 300
 	rndRadius = 0.0400
 	ammodefault = 5
-	
 
-def getRandomPhrase():
-	words = [line.strip() for line in open('words.txt')]
-	return(random.choice(words))
-
-def ship_key(shipkey=DEFAULT_SHIP):
-	"""Constructs a Datastore key for a Ship entity.
+def player_key(shipkey=DEFAULT_SHIP):
+	"""Constructs a Datastore key for a Player entity.
 
 	We use shipname as the key.
 	"""
-	return ndb.Key('Ship', shipkey)
+	return ndb.Key('Player', shipkey)
 
-class Ship(ndb.Model):
+class Player(ndb.Model):
 	"""Model for Playerdata"""
-	shipKey = ship_key()
-	shipCreated = ndb.DateTimeProperty(auto_now_add=True)
-	shipName = ndb.StringProperty(required=True,default="")
-	shipPass = ndb.StringProperty(required=True)
-	shipScore = ndb.IntegerProperty(default=0)
-	shipFaction = ndb.StringProperty(required=True,default="")
+	playerKey = player_key()
+	playerCreated = ndb.DateTimeProperty(auto_now_add=True)
+	playerName = ndb.StringProperty(required=True,default="")
+	playerPW = ndb.StringProperty(required=True)
+	playerScore = ndb.IntegerProperty(default=0)
+	playerTeam = ndb.StringProperty(required=True,default="")
 
 	# list all registerd Ships
-	@classmethod
-	def showAllShips(self):
-		ships = []
-		shipquery = self.query().fetch()
-		for ship in shipquery:
-			showship = {
-			'id': ship.key.id(),
-			'name': ship.shipName,
-			'team': ship.shipFaction,
-			'score': ship.shipScore,
-			}
-			ships.append(showship)
-		return ships
+	# @classmethod
+	# def showAllShips(self):
+	# 	ships = []
+	# 	shipquery = self.query().fetch()
+	# 	for ship in shipquery:
+	# 		showship = {
+	# 		'id': ship.key.id(),
+	# 		'name': ship.shipName,
+	# 		'team': ship.shipFaction,
+	# 		'score': ship.shipScore,
+	# 		}
+	# 		ships.append(showship)
+	# 	return ships
 
 	# show Scores
-	@classmethod
-	def showScores(self):
-		ships = []
-		shipquery = self.query().order(-self.shipScore).fetch()
-		for ship in shipquery:
-			showship = {
-			'name': ship.shipName,
-			'team': ship.shipFaction,
-			'score': ship.shipScore,
-			}
-			ships.append(showship)
-
-		return ships
-
-
-	# check free ShipName
-	@classmethod
-	def checkfreeshipname(self,name):
-		shipquery = self.query(self.shipName == name).fetch(1,keys_only=True)
-		if len(shipquery) == 0:
-			response = {
-			'status': "ACK",
-			'reason': 'Name ok',
-			}
-		else:
-			response = {
-			'status': "NACK",
-			'reason': 'Name already taken',
-			}
-		return response
-
-	@classmethod
-	def loginShip(self,name,secret):
-		shipquery = self.query(ndb.AND(self.shipName == name,self.shipPassCode == secret)).fetch(1,keys_only=True)
-		if len(shipquery) == 1:
-			ship = shipquery[0].get()
-			response = {
-			'status': "ayeaye",
-			'reason': 'Login ok',
-			'team': str(ship.shipFaction),
-			'shipid': str(ship.key.id()),
-			}
-		else:
-			response = {
-			'status': "noayeaye",
-			'reason': 'Login failed',
-			}
-		return response
-
-	# save ShipConfig
-	@classmethod
-	def saveShipConfig(self,name,faction,pos,heading):
-		ship = Ship()
-		phrase = getRandomPhrase()
-		ship.shipName = name
-		ship.shipFaction = faction
-		ship.shipPassCode = phrase
-		shipquery = self.query(self.shipName == name).fetch(1,keys_only=True)
-		if len(shipquery) == 0:
-			ship.put()
-			response = {
-			'status': "ACK",
-			'reason': 'Save ok',
-			'secret': phrase,
-			'shipid': str(ship.key.id()),
-			}
-		else:
-			response = {
-			'status': "NACK",
-			'reason': 'Name was just taken',
-			'passphrase': '',
-			}
-		return response
-
-
-	# create new Ship
-	@classmethod
-	def create_newship(self,name,pos,faction):
-		ship = Ship()
-		ship.shipName = name
-		ship.shipTargetPos = ndb.GeoPt(str(pos))
-		ship.shipFaction = str(faction)
-		ship.put()
-		return ship.key.id()
+	# @classmethod
+	# def showScores(self):
+	# 	ships = []
+	# 	shipquery = self.query().order(-self.shipScore).fetch()
+	# 	for ship in shipquery:
+	# 		showship = {
+	# 		'name': ship.shipName,
+	# 		'team': ship.shipFaction,
+	# 		'score': ship.shipScore,
+	# 		}
+	# 		ships.append(showship)
+	#
+	# 	return ships
+	#
+	#
+	# # check free ShipName
+	# @classmethod
+	# def checkfreeshipname(self,name):
+	# 	shipquery = self.query(self.shipName == name).fetch(1,keys_only=True)
+	# 	if len(shipquery) == 0:
+	# 		response = {
+	# 		'status': "ACK",
+	# 		'reason': 'Name ok',
+	# 		}
+	# 	else:
+	# 		response = {
+	# 		'status': "NACK",
+	# 		'reason': 'Name already taken',
+	# 		}
+	# 	return response
+	#
+	# @classmethod
+	# def loginShip(self,name,secret):
+	# 	shipquery = self.query(ndb.AND(self.shipName == name,self.shipPassCode == secret)).fetch(1,keys_only=True)
+	# 	if len(shipquery) == 1:
+	# 		ship = shipquery[0].get()
+	# 		response = {
+	# 		'status': "ayeaye",
+	# 		'reason': 'Login ok',
+	# 		'team': str(ship.shipFaction),
+	# 		'shipid': str(ship.key.id()),
+	# 		}
+	# 	else:
+	# 		response = {
+	# 		'status': "noayeaye",
+	# 		'reason': 'Login failed',
+	# 		}
+	# 	return response
+	#
+	# # save ShipConfig
+	# @classmethod
+	# def saveShipConfig(self,name,faction,pos,heading):
+	# 	ship = Ship()
+	# 	phrase = getRandomPhrase()
+	# 	ship.shipName = name
+	# 	ship.shipFaction = faction
+	# 	ship.shipPassCode = phrase
+	# 	shipquery = self.query(self.shipName == name).fetch(1,keys_only=True)
+	# 	if len(shipquery) == 0:
+	# 		ship.put()
+	# 		response = {
+	# 		'status': "ACK",
+	# 		'reason': 'Save ok',
+	# 		'secret': phrase,
+	# 		'shipid': str(ship.key.id()),
+	# 		}
+	# 	else:
+	# 		response = {
+	# 		'status': "NACK",
+	# 		'reason': 'Name was just taken',
+	# 		'passphrase': '',
+	# 		}
+	# 	return response
+	#
+	#
+	# # create new Ship
+	# @classmethod
+	# def create_newship(self,name,pos,faction):
+	# 	ship = Ship()
+	# 	ship.shipName = name
+	# 	ship.shipTargetPos = ndb.GeoPt(str(pos))
+	# 	ship.shipFaction = str(faction)
+	# 	ship.put()
+	# 	return ship.key.id()
