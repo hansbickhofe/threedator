dd252920eb5424d7bb6f3ae9a2e1c1e116b8e2cf
bickhofe <peter@bickhofe.de>
2015-10-31 12:38:43 +0100

    unity: settingsmenue ui und ingame data overlay

---
+++ unity/Assets/Scripts/Settings.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Settings : MonoBehaviour {
+
+	public string pName;
+	public string pTeam;
+	public string pID;
+	public string pPW;
+	public string roomID;
+	public string url = "http://defaulturl.com";
+	
+	InputField InputName;
+	InputField InputTeam;
+	InputField InputID;
+	InputField InputPW;
+	InputField InputRoomID;
+	InputField InputUrl;
+
+	// Use this for initialization
+	void Start () {
+		//access input fields
+		InputName = GameObject.Find("InputFieldPlayer").GetComponent<InputField>();
+		InputTeam = GameObject.Find("InputFieldTeam").GetComponent<InputField>();
+		InputID = GameObject.Find("InputFieldID").GetComponent<InputField>();
+		InputPW = GameObject.Find("InputFieldPW").GetComponent<InputField>();
+		InputRoomID = GameObject.Find("InputFieldRoomID").GetComponent<InputField>();
+		InputUrl = GameObject.Find("InputFieldURL").GetComponent<InputField>();
+
+		//set default url
+		InputUrl.placeholder.GetComponent<Text>().text = url;
+	}
+
+	public void SetFormData(){
+		pName = InputName.text;
+		pTeam = InputTeam.text;
+		pID = InputID.text;
+		pPW = InputPW.text;
+		roomID = InputRoomID.text;
+		url = InputUrl.text;
+
+		SaveToLocalPrefs ();
+	}
+
+	void SaveToLocalPrefs(){
+		PlayerPrefs.SetString("PLAYER", pName);
+		PlayerPrefs.SetString("TEAM", pTeam);
+		PlayerPrefs.SetString("ID", pID);
+		PlayerPrefs.SetString("PASSWORD", pPW);
+		PlayerPrefs.SetString("ROOM", roomID);
+		PlayerPrefs.SetString("URL", url);
+	}
+
+	public void StartGame(){
+		Application.LoadLevel ("Threedator");
+	}
+}
---
+++ unity/Assets/Scripts/UI.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour {
+
+	public GameObject DebugTxt;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ToggleGameData(){
+		if (DebugTxt.activeSelf) {
+			DebugTxt.SetActive (false);
+			print ("hide");
+		} else {
+			DebugTxt.SetActive (true);
+			print ("show");
+		}
+	}
+
+	public void EnterSettingsMenu(){
+		print ("hit");
+		Application.LoadLevel ("Settings");
+	}
+}
