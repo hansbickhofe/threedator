ebedbd23eb6995646073d9eca9468ad5d1def063
bickhofe <peter@bickhofe.de>
2015-11-01 22:00:26 +0100

    immer noch im progress

--- unity/Assets/Scripts/TDSocketIO.cs
+++ unity/Assets/Scripts/TDSocketIO.cs
@@ -35,8 +35,10 @@ public class TDSocketIO : MonoBehaviour
 	// received data
 	public GameObject r_ship;
 	int r_id;
-	float r_xPos;
-	float r_zPos;
+	float r_posX;
+	float r_posZ;
+	float r_targetX;
+	float r_targetZ;
 	int r_shipTime;
 	
 	public void Start() {
@@ -49,6 +51,7 @@ public class TDSocketIO : MonoBehaviour
 	public void Update(){
 		//timer
 		timer += Time.deltaTime;
+
 		if (timer > sendDataTime) {
 			//get current target course
 			targetX = TargetScript.targetX;
@@ -81,8 +84,8 @@ public class TDSocketIO : MonoBehaviour
 		//print ("-> "+ jo["id"].str +" "+ jo["xPos"].str +" "+ jo["yPos"].str+" "+ jo["time"].str);
 
 		r_id = int.Parse(jo["id"].str);
-		r_xPos = float.Parse(jo["xPos"].str);
-		r_zPos = float.Parse(jo["yPos"].str);
+		r_posX = float.Parse(jo["posX"].str);
+		r_posZ = float.Parse(jo["posZ"].str);
 		r_shipTime = int.Parse(jo["time"].str);
 		ProcessData();
 		CleanupOldData();
@@ -97,9 +100,9 @@ public class TDSocketIO : MonoBehaviour
 		for (int i = 0; i<arraySize; i++){
 			if ((int)allShips[i].id == r_id) {
 				//existing ship pos updaten
-				allShips[i].xPos = r_xPos;
-				allShips[i].zPos = r_zPos;
-				allShips[i].ship.transform.position = new Vector3(r_xPos,0.5f,r_zPos);
+				allShips[i].posX = r_posX;
+				allShips[i].posZ = r_posZ;
+				allShips[i].ship.transform.position = new Vector3(r_posX,0.5f,r_posZ);
 				allShips[i].time = r_shipTime;
 				print(id+": is already there!");
 				idFound = true;
@@ -111,13 +114,13 @@ public class TDSocketIO : MonoBehaviour
 		if (!idFound){
 			//ship an random pos mit random color erzeugen
 			GameObject newShip;
-			Vector3 spawnPosition = new Vector3(r_xPos,2,r_zPos);
+			Vector3 spawnPosition = new Vector3(r_posX,2,r_posZ);
 			newShip = Instantiate(ship, spawnPosition, transform.rotation) as GameObject;
 			Color randomColor = new Color (UnityEngine.Random.Range(0.0f,1.0f),UnityEngine.Random.Range(0.0f,1.0f),UnityEngine.Random.Range(0.0f,1.0f));
 			newShip.GetComponent<Renderer>().material.SetColor("_Color", randomColor);
 
 			// neue daten ins array schreiben
-			allShips.Add(new Ship(newShip, r_id, r_xPos, r_zPos, r_shipTime));
+			allShips.Add(new Ship(newShip, r_id, r_posX, r_posZ, r_shipTime));
 			print (r_id+": added!");
 			arraySize++;
 		}
