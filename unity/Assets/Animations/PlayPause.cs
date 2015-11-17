fcba693ba3c407b12d206abe1be6e7e89626cec5
bickhofe <peter@bickhofe.de>
2015-11-17 17:46:43 +0100

    teststuff

---
+++ unity/Assets/Animations/PlayPause.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayPause : MonoBehaviour {
+
+	Animator myAnimator;
+
+	// Use this for initialization
+	void Start () {
+		myAnimator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey("space")){
+			myAnimator.SetBool("isMoving", true);
+		} else {
+			myAnimator.SetBool("isMoving", false);
+		}
+	}
+}
