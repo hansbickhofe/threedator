8a004082a034ca99d1ae02b50776c630160bf957
bickhofe <peter@bickhofe.de>
2015-11-30 20:19:32 +0100

    movment interaction fixed

---
+++ unity/Assets/Scripts/AstronautMove.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AstronautMove : MonoBehaviour {
+
+	public Animator MyAnimator;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)) MyAnimator.SetBool("isMoving",true);
+		if(Input.GetMouseButtonUp(0)) MyAnimator.SetBool("isMoving",false);
+	}
+}
